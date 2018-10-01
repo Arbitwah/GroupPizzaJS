758a37956cb8425b5be04a039ae5848a13479182
Greg Pugh <arbitwah@gmail.com>
2018-10-01 09:39:36 -0500

    Group push

--- Cheechs Pizza/pizza.js
+++ Cheechs Pizza/pizza.js
@@ -1,1 +1,10 @@
 pizzaType = ["Pepperoni", "Cheese", "Hawaiian", "Meat Lovers", "Bacon"]
+
+function onLoad(){
+    for(let i = 0; i < pizzaType.length;i++)
+    {
+    document.getElementById("inputState selectPizza").innerHTML += `<option>${pizzaType[i]}</option>`
+    }
+}
+
+window.addEventListener("load",onLoad);
