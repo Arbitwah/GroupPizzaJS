2f528250c3b3209f6801b2c38192fb576beb088d
Greg Pugh <arbitwah@gmail.com>
2018-10-01 10:30:29 -0500

    test commit

--- Cheechs Pizza/pizza.js
+++ Cheechs Pizza/pizza.js
@@ -3,15 +3,11 @@ pizzaType = ["Pepperoni", "Cheese", "Hawaiian", "Meat Lovers", "Bacon"]
 function onLoad(){
     for(let i = 0; i < pizzaType.length;i++)
     {
-    document.getElementById("inputState PizzaType").innerHTML += `<option>${pizzaType[i]}</option>`
+    document.getElementById("inputState selectPizza").innerHTML += `<option>${pizzaType[i]}</option>`
     }
     for(let i = 0; i <= 9; i++){
         document.getElementById("inputState PizzaNumber").innerHTML += `<option>${i + 1}</option>`
     }
 }
 
-
-
 window.addEventListener("load",onLoad);
-
-
