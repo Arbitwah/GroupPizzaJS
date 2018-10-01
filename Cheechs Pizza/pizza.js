74e2fd4684ed2fa8c1fe3fa99a488fa168afb03e
Phillip Swope <ps0895423@otc.edu>
2018-10-01 10:11:25 -0500

    Number of Pizzas Complete

--- Cheechs Pizza/pizza.js
+++ Cheechs Pizza/pizza.js
@@ -3,8 +3,15 @@ pizzaType = ["Pepperoni", "Cheese", "Hawaiian", "Meat Lovers", "Bacon"]
 function onLoad(){
     for(let i = 0; i < pizzaType.length;i++)
     {
-    document.getElementById("inputState selectPizza").innerHTML += `<option>${pizzaType[i]}</option>`
+    document.getElementById("inputState PizzaType").innerHTML += `<option>${pizzaType[i]}</option>`
+    }
+    for(let i = 0; i <= 9; i++){
+        document.getElementById("inputState PizzaNumber").innerHTML += `<option>${i + 1}</option>`
     }
 }
 
+
+
 window.addEventListener("load",onLoad);
+
+
