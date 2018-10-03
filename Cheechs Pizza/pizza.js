c4e65c6f2da579788a152999eb047da23a76bc79
Greg Pugh <arbitwah@gmail.com>
2018-10-02 20:43:31 -0500

    redo inbound

--- Cheechs Pizza/pizza.js
+++ Cheechs Pizza/pizza.js
@@ -8,12 +8,20 @@ function onLoad(){
     for(let i = 0; i <= 9; i++){
         document.getElementById("inputState PizzaNumber").innerHTML += `<option>${i + 1}</option>`
     }
-}
+};
 
 function grabInput(){
     var tabName = document.getElementById("inputName tabName").value;
     var phoneNum = document.getElementById("inputPhone pNum").value;
-    var selectedPizza = document.getElementById("inputState selectPizza").options[selectedIndex].value;
-}
+    //var selectedPizza = document.getElementById("inputState selectPizza").options[selectedIndex].value;
+    
+  
+        document.getElementById("OrderOut").innerHTML += `<p>${tabName}</p>`;
+        document.getElementById("OrderOut").innerHTML += `<p>${phoneNum}</p>`;
+    //    document.getElementById("OrderOut").innerHTML += `<p>${selectedPizza}</p>`;
+    return false;
+
+};
 window.addEventListener("load",onLoad);
-window.addEventListener("click", grabInput);
+document.getElementById("button").addEventListener("click", grabInput);
+
