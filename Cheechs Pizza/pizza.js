8e3773d16da5672112c8e4d4d484b4121459cd8e
Arbitwah <arbitwah@gmail.com>
2018-10-02 22:44:53 -0500

    merge error inboud v4

--- Cheechs Pizza/pizza.js
+++ Cheechs Pizza/pizza.js
@@ -8,20 +8,22 @@ function onLoad(){
     for(let i = 0; i <= 9; i++){
         document.getElementById("PizzaNumber").innerHTML += `<option>${i + 1}</option>`
     }
+    document.getElementById("btnPress").addEventListener("click",grabInput);
 };
 
 function grabInput(){
-    var tabName = document.getElementById("inputName tabName").value;
-    var phoneNum = document.getElementById("inputPhone pNum").value;
-    //var selectedPizza = document.getElementById("inputState selectPizza").options[selectedIndex].value;
+    var tabName = document.getElementById("tabName").value;
+    var phoneNum = document.getElementById("pNum").value;
+    var selectedPizza = document.getElementById("selectPizza").options[selectedIndex].value;
     
   
-        document.getElementById("OrderOut").innerHTML += `<p>${tabName}</p>`;
-        document.getElementById("OrderOut").innerHTML += `<p>${phoneNum}</p>`;
-    //    document.getElementById("OrderOut").innerHTML += `<p>${selectedPizza}</p>`;
+        document.getElementById("OrderOut").innerHTML += `${tabName}\n`;
+        document.getElementById("OrderOut").innerHTML += `${phoneNum}\n`;
+        document.getElementById("OrderOut").innerHTML += `${selectedPizza}`;
     return false;
 
 };
+
 window.addEventListener("load",onLoad);
-document.getElementById("button").addEventListener("click", grabInput);
+
 
