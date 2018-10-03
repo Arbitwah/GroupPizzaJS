434088386a97e83c04dd664e91f34009effe2517
Arbitwah <arbitwah@gmail.com>
2018-10-02 21:52:49 -0500

    Redone Project using w3schools example code

--- Cheechs Pizza/pizza.js
+++ Cheechs Pizza/pizza.js
@@ -3,10 +3,10 @@ pizzaType = ["Pepperoni", "Cheese", "Hawaiian", "Meat Lovers", "Bacon"]
 function onLoad(){
     for(let i = 0; i < pizzaType.length;i++)
     {
-    document.getElementById("inputState selectPizza").innerHTML += `<option>${pizzaType[i]}</option>`
+    document.getElementById("selectPizza").innerHTML += `<option>${pizzaType[i]}</option>`
     }
     for(let i = 0; i <= 9; i++){
-        document.getElementById("inputState PizzaNumber").innerHTML += `<option>${i + 1}</option>`
+        document.getElementById("PizzaNumber").innerHTML += `<option>${i + 1}</option>`
     }
 };
 
